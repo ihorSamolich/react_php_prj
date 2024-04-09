a0718b16be8ddb4fcebd2db36c6c7deff8227dae
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-04-09 18:10:13 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./index.css";
 import Sidebar from "./components/Sidebar.tsx";
 import Footer from "./components/Footer.tsx";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import UsersTasks from "./pages/UsersTasks.tsx";
+import TasksPage from "./pages/TasksPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
 
 export default function App() {
   console.log("render app");
@@ -9,6 +14,15 @@ export default function App() {
     <div className="font-body">
       <Sidebar />
       <div className="md:ml-64">
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />} />
+            <Route path="users" element={<UsersTasks />} />
+            <Route path="tasks" element={<TasksPage />} />
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
         <Footer />
       </div>
     </div>
---
+++ src/pages/UsersTasks.tsx
@@ -0,0 +1,5 @@
+const UsersTasks = () => {
+  return <div>UsersTasks</div>;
+};
+
+export default UsersTasks;
